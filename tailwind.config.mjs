df4d08e1fd05297a5e6c95dead94cbf4366197cd
laloxox <lalopx555@gmail.com>
2024-12-27 17:37:58 -0600

    Se crearon los diseños de las diferentes secciones del proyecto con HTML y Tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#473EC3',
+				secundary: '#9C9DEC',
+				accent: '',
+				dark: ''
+			}
+		},
 	},
 	plugins: [],
 }
